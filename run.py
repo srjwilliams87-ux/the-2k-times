da3a8922b190dfa061272e120727efea0276bcfd
srjwilliams87-ux <srjwilliams87@gmail.com>
2026-01-11 21:29:22 +0000

    Update run.py

--- run.py
+++ run.py
@@ -340,7 +340,9 @@ def render_story(s, idx):
 </body>
 </html>
 """
-    return html
+    if 'html' not in locals():
+        raise RuntimeError("render_email(): html was never created")
+    return html        
 
 # --------------------------------------------------
 # Mailgun
