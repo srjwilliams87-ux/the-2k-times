dc4ea8f0f7af2039f24fca72446dab54f3a04a4f
srjwilliams87-ux <srjwilliams87@gmail.com>
2026-02-07 14:20:42 +0000

    Update run.py

--- run.py
+++ run.py
@@ -452,7 +452,7 @@ def build_email_html(world, weather, sun, people, edition_tag="v-newspaper-17") 
     """
 
     # Who's in space (mirror whoisinspace.com: mission + time on mission)
-    if people:
+if people:
     # people is expected to be list[dict] with: name, mission, launched_utc
     now = dt.datetime.now(dt.timezone.utc)
 
